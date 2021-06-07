4f4d1d05f8ab2b390fb2cd6da73cf61b71ac549c
Eric van Zandvoort <gitlabs@frotmail.nl>
2021-06-07 16:37:24 +0200

    influxdb-v2 compatible

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 from threading import Timer
-from influxdb import InfluxDBClient
+from influxdb_client import InfluxDBClient, Point
 from goodwe import Goodwe
 
 def fillInflux():
     '''
     Retrieve stats and insert into influx
     '''
-    global gw, influx_host, influx_user, influx_pass, influx_port, influx_db, influx_tls
+    global gw, influxdb_org, influxdb_bucket
     try:
         retries = 2
         data = gw.get_inverter_data()
@@ -16,9 +16,10 @@ def fillInflux():
             retries -= 1
             data = gw.get_inverter_data()
         if(data['error'] == 'no error'):
-            db = InfluxDBClient(influx_host,influx_port,influx_user,influx_pass,influx_db, ssl=influx_tls)
-            jso = [ {"measurement": "goodwe", "tags":{"source": "Python" }, "fields": data } ]
-            db.write_points(jso)
+            db = InfluxDBClient.from_env_properties()
+            with db.write_api() as write_api:
+                jso = [ {"measurement": "goodwe", "tags":{"source": "Python" }, "fields": data } ]
+                write_api.write(influxdb_bucket, influxdb_org, jso)
             db.close()
             print("Written to db:",data)
         else:
@@ -27,14 +28,8 @@ def fillInflux():
         Timer(10,fillInflux).start()
 
 inverter_host = os.environ['INVERTER_HOST']
-influx_host = os.environ.get('INFLUX_HOST','localhost')
-influx_user = os.environ.get('INFLUX_USER','')
-influx_pass = os.environ.get('INFLUX_PASS','')
-influx_port = int(os.environ.get('INFLUX_PORT',8086))
-influx_db = os.environ.get('INFLUX_DB','default')
-tls = os.environ.get('INFLUX_TLS','false')
-influx_tls = ("true" in tls) or ("True" in tls)
-
+influxdb_bucket = os.environ['INFLUXDB_V2_BUCKET']
+influxdb_org = os.environ['INFLUXDB_V2_ORG']
 gw = Goodwe(inverter_host)
 
 fillInflux()
